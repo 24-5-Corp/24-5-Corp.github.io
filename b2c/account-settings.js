c12d3176af1fb3985182362ab46bfbb2155751fe
moonyaho <moonp@ssgsag.kr>
2023-07-14 16:11:29 +0900

    [SSGSAG-758] 슈퍼패스 B2C 현황 작업

---
+++ b2c/account-settings.js
@@ -0,0 +1,41 @@
+const $emailText = document.querySelector(".email-text");
+const $withdrawalButton = document.querySelector(".withdrawal-button");
+
+apiService
+  .makeRequest("/auth/b2c/me")
+  .then((response) => {
+    $emailText.textContent = response.data.email;
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
+const leaveCheckModal = new ConfirmModal(
+  document.querySelector(".leave-check-modal")
+);
+const leaveDoneModal = new AlertModal(
+  document.querySelector(".leave-done-modal")
+);
+
+$withdrawalButton.addEventListener("click", () => {
+  leaveCheckModal.handleShow(true);
+});
+
+leaveCheckModal.onConfirm = () => {
+  apiService
+    .makeRequest("/auth/b2c/withdrawal")
+    .then(() => {
+      leaveDoneModal.handleShow(true);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
+
+leaveDoneModal.onCheck = () => {
+  location.href = "/";
+};
+
+Webflow.push(() => {
+  document.querySelector("#account").classList.add("current");
+});
