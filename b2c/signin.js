b5864961d3c51069a2cc325924a95f1b33688ad1
moonyaho <moonp@ssgsag.kr>
2023-07-14 15:52:32 +0900

    [SSGSAG-758] 슈퍼패스 B2C 현황 작업

---
+++ b2c/signin.js
@@ -0,0 +1,42 @@
+const params = new URLSearchParams(location.search);
+const code = params.get("code");
+
+apiService
+  .makeRequest(`/auth/b2c/authrization?code=${code}`)
+  .then((response) => {
+    const { accessToken, refreshToken } = response;
+    localStorage.setItem("accessToken", accessToken);
+    localStorage.setItem("refreshToken", refreshToken);
+
+    const applyData = localStorage.getItem("applyData");
+    if (applyData) {
+      apiService
+        .makeRequest("/superpass/v2/apply-seeker", {
+          method: "POST",
+          body: applyData,
+        })
+        .then((_) => {
+          fbq("track", "SubmitApplication");
+          logScreenView({ screenName: "superpass_apply_popup_submit" });
+          localStorage.removeItem(applyData);
+          location.href = "/matches";
+        })
+        .catch((error) => console.error(error));
+    } else {
+      const loginUrl = localStorage.getItem("loginUrl");
+
+      apiService
+        .makeRequest("/superpass/v2/apply-seeker", {
+          method: "GET",
+        })
+        .then((response) => {
+          if (response.data === null) {
+            location.href = `${loginUrl}?isSigned=true`;
+          } else {
+            location.href = "/matches";
+          }
+        })
+        .catch((error) => console.error(error));
+    }
+  })
+  .catch((error) => console.error(error));
