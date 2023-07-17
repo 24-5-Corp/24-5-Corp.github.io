8db0c2d25397a5f2bc4ca0e72292b649359806f2
박민지 <minji@ssgsag.kr>
2023-07-17 18:42:43 +0900

    [SSGSAG-778] 지원서 조회 뷰 api 호출 및 바인딩

--- b2c/my-application.js
+++ b2c/my-application.js
@@ -291,7 +291,7 @@ class MyAppicationView {
 
     this._workStartDate.textContent = parseDate(workCondition.workStart, true);
 
-    const additional = !workCondition.additional ?? "없음";
+    const additional = workCondition.additional ?? "없음";
     this._workAdditional.textContent = additional;
 
     this.handleShow(true);
